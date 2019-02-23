b7c6c77ec5cba57bb1b25af31dad8151c3c8987e
root <root@ip-172-31-44-8.us-east-2.compute.internal>
2019-02-23 07:31:46 +0000

    first commit

---
+++ build/precache-manifest.f26fea3660ca601d29baa5beb5d5f779.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "37be9f5431f99b47e463",
+    "url": "/static/js/main.37be9f54.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "37be9f5431f99b47e463",
+    "url": "/static/css/main.2afbe5a7.chunk.css"
+  },
+  {
+    "revision": "5cf73259920246fa5609908010ccc8a1",
+    "url": "/index.html"
+  }
+];
